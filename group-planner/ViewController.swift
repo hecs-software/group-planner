347163b678935202be06a2a895dfeb3f07c34bcc
Christopher Guan <cwguan@ucsd.edu>
2018-03-01 21:28:31 -0800

    Initial Commit

---
+++ group-planner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  group-planner
+//
+//  Created by Christopher Guan on 3/1/18.
+//  Copyright © 2018 Christopher Guan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
