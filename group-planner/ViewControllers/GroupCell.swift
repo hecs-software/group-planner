03a699a080df71fd49647c94752fbc2a7abf0285
Christopher Guan <cwguan@ucsd.edu>
2018-04-29 16:02:53 -0700

    Created tableview and tableview cells for grouplist

---
+++ group-planner/ViewControllers/GroupCell.swift
@@ -0,0 +1,28 @@
+//
+//  GroupCell.swift
+//  group-planner
+//
+//  Created by Christopher Guan on 4/29/18.
+//  Copyright © 2018 Christopher Guan. All rights reserved.
+//
+
+import UIKit
+
+class GroupCell: UITableViewCell {
+    
+    var group: Group!
+    @IBOutlet weak var groupNameLabel: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ group-planner/ViewControllers/GroupListViewController.swift
@@ -0,0 +1,38 @@
+//
+//  GroupListViewController.swift
+//  group-planner
+//
+//  Created by Christopher Guan on 4/29/18.
+//  Copyright © 2018 Christopher Guan. All rights reserved.
+//
+
+import UIKit
+
+class GroupListViewController: UIViewController, UITableViewDataSource {
+
+    @IBOutlet weak var groupsTableView: UITableView!
+    var groups: [Group] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return groups.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "GroupCell", for: indexPath) as! GroupCell
+        let group = groups[indexPath.row]
+        cell.group = group
+        return cell
+    }
+    
+    
+}
